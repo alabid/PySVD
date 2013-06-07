a70926a344d74d277bee576ff3ea17467e3ce226
wangc <wangc@cmc301-11.mathcs.carleton.edu>
2013-06-06 20:01:29 -0500

    graphs and analyses added

---
+++ plots.py
@@ -0,0 +1,147 @@
+# Daniel Alabi and Cody Wang
+# Separate code for plotting since matplotlib runs slow under Python
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import spline
+
+if __name__ == "__main__":
+    
+    # ========= Code to Plot time against k ========= #
+    f = open("ranktime2")
+    kvalues = range(5,56)
+    lines = f.readlines()
+    line1 = lines[0].split('\t')
+    times = [float(line1[i]) for i in range(0, len(line1)-1)]
+    
+    xdata = np.array(kvalues)
+    ydata = np.array(times)    
+    
+
+    plt.xlabel("k-values")
+    plt.ylabel("time used")
+
+    xnew = np.linspace(xdata.min(), xdata.max(), len(kvalues)*1)
+    ysmooth = spline(xdata, ydata, xnew)
+    
+    plt.plot(xnew, ysmooth)
+    plt.show()
+    # ==================================================================#
+    
+    # ========= Code to Plot time against regularizers ========= #
+    f = open("regularizertime2")
+    regularizers = [i*0.001 for i in range(1,51)]
+    lines = f.readlines()
+    line1 = lines[0].split('\t')
+    times = [float(line1[i]) for i in range(0, len(line1)-1)]
+    
+    xdata = np.array(regularizers)
+    ydata = np.array(times)    
+    
+
+    plt.xlabel("regularizers")
+    plt.ylabel("time used")
+
+    xnew = np.linspace(xdata.min(), xdata.max(), len(regularizers)*1)
+    ysmooth = spline(xdata, ydata, xnew)
+    
+    plt.plot(xnew, ysmooth)
+    plt.show()
+    # ==================================================================#
+    
+    # ========= Code to Plot time against lrates ========= #
+    f2 = open("lratetime2")
+    lrate = [i*0.0005 for i in range(1, 51)]
+    lines = f2.readlines()
+    line1 = lines[0].split('\t')
+    times = [float(line1[i]) for i in range(0, len(line1)-1)]
+    
+    xdata = np.array(lrate)
+    ydata = np.array(times)    
+    
+
+    plt.xlabel("learning rate")
+    plt.ylabel("time used")
+
+    xnew = np.linspace(xdata.min(), xdata.max(), len(lrate)*1)
+    ysmooth = spline(xdata, ydata, xnew)
+    
+    plt.plot(xnew, ysmooth)
+    plt.show()
+    # ==================================================================#
+
+    # ========= Code to Plot rmse against k ========= #
+    f3 = open("ranktest2")
+    kvalues = range(5,56)
+    lines = f3.readlines()
+    line1 = lines[0].split('\t')
+    rmsetrain = [float(line1[i]) for i in range(0, len(line1)-1)]
+    line2 = lines[1].split('\t')
+    rmsetest = [float(line2[i]) for i in range(0, len(line2)-1)]
+    
+    xdata = np.array(kvalues)
+    y1data = np.array(rmsetrain)    
+    y2data = np.array(rmsetest)
+
+    plt.xlabel("k-values")
+    plt.ylabel("rmse of data (red: trainingset; blue: testset)")
+
+    xnew = np.linspace(xdata.min(), xdata.max(), len(kvalues)*1)
+    ysmooth = spline(xdata, y1data, xnew)
+    y2smooth = spline(xdata, y2data, xnew)
+    
+    plt.plot(xnew, ysmooth, 'r--', xnew, y2smooth, 'bs')
+    plt.show()
+    # ==================================================================#
+   
+
+    # ========= Code to Plot rmse against regularizers ========= #
+    f2 = open("regularizertest2")
+    regularizers = [i*0.001 for i in range(1,51)]
+    lines = f2.readlines()
+    line1 = lines[0].split('\t')
+    rmsetrain = [float(line1[i]) for i in range(0, len(line1)-1)]
+    line2 = lines[1].split('\t')
+    rmsetest = [float(line2[i]) for i in range(0, len(line2)-1)]
+
+   
+    xdata = np.array(regularizers)
+    y1data = np.array(rmsetrain)    
+    y2data = np.array(rmsetest)
+
+    plt.xlabel("regularizers")
+    plt.ylabel("rmse of data (red: trainingset; blue: testset)")
+
+    xnew = np.linspace(xdata.min(), xdata.max(), len(regularizers)*1)
+    ysmooth = spline(xdata, y1data, xnew)
+    y2smooth = spline(xdata, y2data, xnew)
+    
+    plt.plot(xnew, ysmooth, 'r--', xnew, y2smooth, 'bs')
+    plt.show()
+    # ==================================================================#
+    
+
+    # ========= Code to Plot rmse against lrate ========= # 
+    f3 = open("lratetest2")
+    lrate = [i*0.0005 for i in range(1, 51)]
+    lines = f3.readlines()
+    line1 = lines[0].split('\t')
+    rmsetrain = [float(line1[i]) for i in range(0, len(line1)-1)]
+    line2 = lines[1].split('\t')
+    rmsetest = [float(line2[i]) for i in range(0, len(line2)-1)]
+   
+    xdata = np.array(lrate)
+    y1data = np.array(rmsetrain)
+    y2data = np.array(rmsetest)
+
+    plt.xlabel("lrate")
+    plt.ylabel("rmse of data (red: trainingset; blue: testset)")
+
+    xnew = np.linspace(xdata.min(), xdata.max(), len(lrate)*1)
+    ysmooth = spline(xdata, y1data, xnew)
+    y2smooth = spline(xdata, y2data, xnew)
+    
+    plt.plot(xnew, ysmooth, 'r--', xnew, y2smooth, 'bs')
+    plt.show()
+    # ==================================================================#
+
--- regularizedSVD2.py
+++ regularizedSVD2.py
@@ -7,7 +7,7 @@ import time
 # remember to normalize by subtracting out averages
 
 class SvdMatrix:
-    def __init__(self, trainfile, nusers, nmovies, r):
+    def __init__(self, trainfile, nusers, nmovies, r=10, lrate=0.001, regularizer=0.015):
         self.M = [[None]*nmovies for i in range(nusers)]
         self.Mtest = [[None]*nmovies for i in range(nusers)]
 
@@ -30,10 +30,10 @@ class SvdMatrix:
         self.U = [[initval]*r for i in range(nusers)]
         self.V = [[initval]*r for i in range(nmovies)]
         self.r = r
-        self.lrate = 0.001
-        self.regularizer = 0.011
+        self.lrate = lrate
+        self.regularizer = regularizer
         self.minimprov = 0.001
-        self.maxepochs = 10            
+        self.maxepochs = 30            
 
     def dotproduct(self, v1, v2):
         return sum([v1[i]*v2[i] for i in range(len(v1))])
@@ -128,14 +128,93 @@ class SvdMatrix:
         
                                                
 if __name__ == "__main__":
-    init = time.time()
-    svd = SvdMatrix("ua.base", 943, 1682, 20)
-    svd.trainratings()
-    print "took: ", time.time()-init
+    timeused = []
+    rmsetrain = []
+    rmsetest = []
+    # init = time.time()
+    # svd = SvdMatrix("ua.base", 943, 1682, 40)
+    # svd.trainratings()
+    # print "took: ", time.time()-init
 
-    print svd.calcrmse(svd.M)
-    svd.test("ua.test")
-    print svd.calcrmse(svd.Mtest)
+    # print svd.calcrmse(svd.M)
+    # svd.test("ua.test")
+    # print svd.calcrmse(svd.Mtest)
 
-# 2.76364392142
-# 2.7689925966
+    '''
+
+    # ==============Varying learning rate=============== #
+    lrate = [i*0.0005 for i in range(1, 51)]
+    for l in lrate:
+        init = time.time()
+        svd = SvdMatrix("ua.base", 943, 1682, 13, l)
+        svd.trainratings()
+        rmsetrain.append(svd.calcrmse(svd.M))
+        svd.test("ua.test")
+        rmsetest.append(svd.calcrmse(svd.Mtest))
+        timeused.append(time.time()-init)
+    
+    f = open("lratetest2", 'w')
+    for item in rmsetrain:
+        f.writelines("%s\t" % item)
+    f.writelines('\n')
+    for item in rmsetest:
+        f.writelines("%s\t" % item)
+    f.close()
+    
+    f2 = open("lratetime2", 'w')
+    for item in timeused:
+        f2.writelines("%s\t" % item)
+    f2.close()
+
+    # ================================================== #
+    
+    # ==============Varying regularizer================= #
+    regularizers = [i*0.001 for i in range(1, 51)]
+    for reg in regularizers:
+        init = time.time()
+        svd = SvdMatrix("ua.base", 943, 1682, 13, 0.001, reg)
+        svd.trainratings()
+        rmsetrain.append(svd.calcrmse(svd.M))
+        svd.test("ua.test")
+        rmsetest.append(svd.calcrmse(svd.Mtest))
+        timeused.append(time.time()-init)
+    
+    f = open("regularizertest2", 'w')
+    for item in rmsetrain:
+        f.writelines("%s\t" % item)
+    f.writelines('\n')
+    for item in rmsetest:
+        f.writelines("%s\t" % item)
+    f.close()
+    
+    f2 = open("regularizertime2", 'w')
+    for item in timeused:
+        f2.writelines("%s\t" % item)
+    f2.close()
+    # ================================================== #
+    
+    '''
+
+    # ==============Varying rank================= #
+    kvalues = range(5, 56)
+    for k in kvalues:
+        init = time.time()
+        svd = SvdMatrix("ua.base", 943, 1682, k)
+        svd.trainratings()
+        rmsetrain.append(svd.calcrmse(svd.M))
+        svd.test("ua.test")
+        rmsetest.append(svd.calcrmse(svd.Mtest))
+        timeused.append(time.time()-init)
+    
+    f = open("ranktest2", 'w')
+    for item in rmsetrain:
+        f.writelines("%s\t" % item)
+    f.writelines('\n')
+    for item in rmsetest:
+        f.writelines("%s\t" % item)
+    f.close()
+    
+    f2 = open("ranktime2", 'w')
+    for item in timeused:
+        f2.writelines("%s\t" % item)
+    f2.close()
